1574a4ab159f29fcd3a80c4ef34b17578f0e090b
Vladyslav Zamotailo <zamotvlad@gmail.com>
2026-09-24 15:25:57 +0300

    fix: correct wsgi.py settings fallback; add Procfile for Heroku

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 
+import environ
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(BASE_DIR / ".env")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 application = get_wsgi_application()
