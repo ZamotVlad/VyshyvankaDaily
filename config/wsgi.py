202d3e6cb80951e942b96a27096f944372bf4957
Vladyslav Zamotailo <zamotvlad@gmail.com>
2026-09-25 12:43:32 +0000

    fix: default to production settings in wsgi/asgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -16,6 +16,6 @@ from django.core.wsgi import get_wsgi_application
 BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(BASE_DIR / ".env")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 application = get_wsgi_application()
